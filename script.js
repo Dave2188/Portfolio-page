012c901b1f97168f69afa3c3f917f4762cfbb55f
David fox <davidfox967@yahoo.com>
2022-08-15 20:47:53 -0600

    changes

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
-let menuBtn = document.getElementById('ham__btn');
-let exMenu = document.getElementById('nav__menu');
-let closeMenu = document.getElementById('head');
+let menuBtn = document.getElementById("ham__btn");
+let exMenu = document.getElementById("nav__menu");
+let closeMenu = document.getElementById("head");
 
-
-function hamShow() {
-  if(exMenu.style.display === 'none') {
-    exMenu.style.display = 'block'
-  }else {
-    exMenu.style.display = 'none'
-  }
+let hamShow = () => {
+	if (exMenu.style.display === "none") {
+		exMenu.style.display = "block";
+		menuBtn.style.transform = `rotate(-90deg)`;
+	} else if (exMenu.style.display === "block") {
+		exMenu.style.display = "none";
+		menuBtn.style.transform = `rotate(0deg)`;
+	}
 };
 
-menuBtn.addEventListener('click',hamShow);
+menuBtn.addEventListener("click", hamShow);
 
 function extraClose() {
-    exMenu.style.display = 'none'
+	if (menuBtn.style.transform === `rotate(-90deg)`) {
+		menuBtn.style.transform = `rotate(0deg)`;
+	}
+	exMenu.style.display = "none";
 }
 
-closeMenu.addEventListener('click',extraClose);
+closeMenu.addEventListener("click", extraClose);
